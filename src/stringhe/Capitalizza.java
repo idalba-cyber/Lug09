6f5daa63d1b20c1da9140f6fff2ca1356c9e795b
Isabella <67A@A67>
2026-07-09 12:28:16 +0200

    esercizi

---
+++ src/stringhe/Capitalizza.java
@@ -0,0 +1,24 @@
+package stringhe;
+
+public class Capitalizza {
+	
+	public String nome, cognome;
+	
+	public void inizialeMaiuscola() {
+		//recupero le iniziali e le separo dal resto 
+		String inizialeNome = nome.substring(0, 1); // estraggo dalla posizione 0 fino alla posizione 1 esclusa
+		String restoNome = nome.substring(1); //estraggo dalla posizione 1 fino a fine String
+		String inizialeCognome = cognome.substring(0, 1);
+		String restoCognome = cognome.substring(1);
+		
+		//manipolazione delle String
+		inizialeNome = inizialeNome.toUpperCase(); //iniziale nome trasformata in maiuscolo se necessario
+		restoNome = restoNome.toLowerCase(); //resto del nome trasformato in minuscolo se necessario 
+		inizialeCognome = inizialeCognome.toUpperCase();
+		restoCognome = restoCognome.toLowerCase(); 
+		nome = inizialeNome + restoNome;
+		cognome = inizialeCognome + restoCognome;
+		System.out.println("Ti chiami: " + nome + " " + cognome);
+	}
+
+}
---
+++ src/stringhe/CapitalizzaTest.java
@@ -0,0 +1,14 @@
+package stringhe;
+
+public class CapitalizzaTest {
+
+	public static void main(String[] args) {
+		Capitalizza mioCap = new Capitalizza();
+		
+		mioCap.nome = "isabella";
+		mioCap.cognome = "DALBA";
+		mioCap.inizialeMaiuscola();
+
+	}
+
+}
