ee5c766182f7a1e5ee84849d15dcb30793fa7ebb
Isabella <idalba@studenti.apuliadigital.it>
2026-07-12 15:56:13 +0200

    conto bancario aggiornato

--- src/contoBancario/ContoBancario.java
+++ src/contoBancario/ContoBancario.java
@@ -1,7 +1,7 @@
 package contoBancario;
 
 public class ContoBancario {
-	public double saldoIniziale;
+	/* public double saldoIniziale;
 	public double versamento; 
 	public double saldoVersamento;
 	public double prelievo, saldoPrelievo;
@@ -21,5 +21,24 @@ public class ContoBancario {
 	
 	public void saldoInteresse() {
 		saldoInteresse = saldoPrelievo+(saldoPrelievo * interesse /100);
+	} */
+	// AGGIORNATO CON MENO VARIABILI
+	public double saldo;
+	public double importo;
+	public int interesse;
+	
+	public void versamenti() {
+		saldo = saldo  + importo;
+		System.out.println("Saldo aggiornato: " + saldo );
+		
+	}
+	
+	public void prelievi() {
+		saldo  = saldo - importo;
+		System.out.println("Saldo aggiornato: " + saldo );
+	}
+	
+	public void saldoInteresse() {
+		saldo = saldo +(saldo * interesse /100);
 	}
 }
--- src/contoBancario/ContoBancarioTest.java
+++ src/contoBancario/ContoBancarioTest.java
@@ -8,30 +8,30 @@ public class ContoBancarioTest {
 		
 
 		System.out.println("ESTRATTO CONTO BANCARIO CLIENTE 1:");
-		cliente1.saldoIniziale = 100;
-		System.out.println("Saldo iniziale: " + cliente1.saldoIniziale + " euro");
-		cliente1.versamento = 200;
-		System.out.println("Versamento: " + cliente1.versamento + " euro");
+		cliente1.saldo = 100;
+		System.out.println("Saldo iniziale: " + cliente1.saldo + " euro");
+		cliente1.importo = 200;
+		System.out.println("Versamento: " + cliente1.importo + " euro");
 		cliente1.versamenti();
-		cliente1.prelievo = 50;
-		System.out.println("Prelievo: " + cliente1.prelievo + " euro");
+		cliente1.importo = 50;
+		System.out.println("Prelievo: " + cliente1.importo + " euro");
 		cliente1.interesse = 2;
 		cliente1.prelievi();
 		cliente1.saldoInteresse();
-		System.out.println("Saldo con interesse del " + cliente1.interesse + "% : " + cliente1.saldoInteresse + " euro");
+		System.out.println("Saldo con interesse del " + cliente1.interesse + "% : " + cliente1.saldo+ " euro");
 		
 		System.out.println("\nESTRATTO CONTO BANCARIO CLIENTE 2:");
-		cliente2.saldoIniziale = 300;
-		System.out.println("Saldo iniziale: " + cliente2.saldoIniziale + " euro");
-		cliente2.versamento = 50;
-		System.out.println("Versamento: " + cliente2.versamento + " euro");
-		cliente2.versamenti();
-		cliente2.prelievo = 100;
-		System.out.println("Prelievo: " + cliente2.prelievo + " euro");
+		cliente2.saldo = 300;
+		System.out.println("Saldo iniziale: " + cliente2.saldo + " euro");
+		cliente2.importo = 80;
+		System.out.println("Prelievo: " + cliente2.importo + " euro");
 		cliente2.prelievi();
+		cliente2.importo = 100;
+		System.out.println("Versamento: " + cliente2.importo + " euro");
+		cliente2.versamenti();
 		cliente2.interesse = 5;
 		cliente2.saldoInteresse();
-		System.out.println("Saldo con interesse del " + cliente2.interesse + "% : " + cliente2.saldoInteresse + " euro");
+		System.out.println("Saldo con interesse del " + cliente2.interesse + "% : " + cliente2.saldo + " euro");
 		
 	}
 
